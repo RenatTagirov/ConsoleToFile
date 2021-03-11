7bd50bc56aa33bea21b861c36720c9a62a44b561
RenatTagitov <rena.tagirov@gmail.com>
2021-03-11 14:21:57 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String destinationFileName = "File.txt";
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(destinationFileName));
+
+        StringBuilder builder = new StringBuilder();
+
+        String s = "";
+        while (!s.equals("exit")) {
+            s = reader.readLine();
+            builder.append(s).append("\n");
+        }
+
+        writer.write(builder.toString());
+        writer.close();
+    }
+}
